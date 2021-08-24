cf04342a3e9b238bab3125f4e74b247743967dd3
natrimore <jakhongir.murtazayev@gmail.com>
2021-08-24 07:30:49 +0500

    feat: add and use command pattern

---
+++ src/jumpbot/Commands/Abstract/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace jumpbot.Commands.Abstract
+{
+    public interface ICommand
+    {
+        Task ExecuteAsync(Message message);
+    }
+}
---
+++ src/jumpbot/Commands/Concrete/StartCommand.cs
@@ -0,0 +1,22 @@
+﻿using jumpbot.Client;
+using jumpbot.Commands.Abstract;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace jumpbot.Commands.Concrete
+{
+    public class StartCommand : ICommand
+    {
+        private readonly ITelegramBotClient _telegramBotClient;
+
+        public StartCommand(ITelegramClient telegramClient)
+        {
+            _telegramBotClient = telegramClient.GetInstance();
+        }
+        public async Task ExecuteAsync(Message message)
+        {
+            await _telegramBotClient.SendTextMessageAsync(message.Chat.Id, "Hello");
+        }
+    }
+}
