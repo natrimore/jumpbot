d7d4668884f0e48aafd3d73698c6325135f3dfd2
natrimore <jakhongir.murtazayev@gmail.com>
2021-08-24 07:30:09 +0500

    feat: use factory pattern for commands

---
+++ src/jumpbot/CommandFactory/ITelegramCommandFactory.cs
@@ -0,0 +1,15 @@
+﻿using jumpbot.Commands.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace jumpbot.CommandFactory
+{
+    public interface ITelegramCommandFactory
+    {
+        ICommand CreateCommand(Message command);
+    }
+}
---
+++ src/jumpbot/CommandFactory/TelegramCommandFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using jumpbot.Commands.Abstract;
+using jumpbot.Commands.Concrete;
+using Telegram.Bot.Types;
+using System;
+
+namespace jumpbot.CommandFactory
+{
+    public class TelegramCommandFactory : ITelegramCommandFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public TelegramCommandFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider ??
+                throw new ArgumentNullException(nameof(serviceProvider));
+        }
+        public ICommand CreateCommand(Message command)
+        {
+            return command.Text switch
+            {
+                "/start" => _serviceProvider.GetRequiredService<StartCommand>(),
+                _ => _serviceProvider.GetRequiredService<StartCommand>()
+            };
+        }
+    }
+}
